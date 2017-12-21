6e1671f2f6661f5bb21e1292c29eb3bc5edbbfe6
Krishtof-Korda <krishtofkorda+git@gmail.com>
2017-12-20 22:51:14 -0800

    add call to luca's LightDetectionAndClassification and perform inference to return the traffic light color ID.

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,10 +1,16 @@
 from styx_msgs.msg import TrafficLight
+from python.infer import LightDetectionAndClassification
+from detection import ImageWrap
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
+        #KK Inprogress-TODO load classifier
+        
+        #KK Line for loading Luca's classifier
+        self.ldac = LightDetectionAndClassification()
+        
         pass
-
+        
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
 
@@ -15,5 +21,11 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        #KK Inprogress-TODO implement light color prediction
+        
+        #KK lines for infering color of the traffic light
+        self.box, self.predictions, self.predicted_class, self.predicted_label , self.annotated_img, self.traffic_light = self.ldac.infer(ImageWrap(image), annotate=True, desired_labels=["Red", "Green", "Yellow"])
+        
+        label_to_id_map = {"Green": 2, "Yellow": 1, "Red": 0, "Off": 4}
+        self.COLORID = label_to_id_map[self.predicted_label]
+        return TrafficLight.self.COLORID
